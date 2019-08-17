a6e32020e41bb9f41704e9a5561ee0bf8ac89888
Gabriel Rabelo <31012788+gabrielrab@users.noreply.github.com>
2019-08-17 15:59:40 -0300

    add icons from "busca" page

--- src/pages/Busca.js
+++ src/pages/Busca.js
@@ -3,7 +3,11 @@ import { Link } from "react-router-dom";
 
 import "./Busca.css";
 
+//Imagens
 import logo from "../assets/logo.svg";
+import car from "../assets/car.svg";
+import bed from "../assets/bed.svg";
+import full-size from "../assets/full-size.svg";
 
 export default function Busca() {
   return (
@@ -14,15 +18,55 @@ export default function Busca() {
             <img src={logo} alt="Nexo Imobiliária" />
           </Link>
         </div>
-        <ul>
-          <li>
-            O que você está procurando ?
-            <label>
-              <button>Alugar</button>
-              <button>Comprar</button>
-            </label>
-          </li>
-        </ul>
+        <div className="search">
+          <ul>
+            <li>
+              O que você está procurando ?<br />
+              <label>
+                <button>Alugar</button>
+                <button>Comprar</button>
+              </label>
+            </li>
+
+            <li>
+              Em qual local ?<br />
+              <label>
+                <select>
+                  <option>Selecione</option>
+                </select>
+              </label>
+            </li>
+
+            <li>
+              O qual tipo de imóvel ?<br />
+              <label>
+                <select>
+                  <option>Selecione</option>
+                </select>
+              </label>
+            </li>
+
+            <li>
+              <img src={car} alt="Carro" className="icone" />
+            </li>
+
+            <li>
+              O que você está procurando ?<br />
+              <label>
+                <button>Alugar</button>
+                <button>Comprar</button>
+              </label>
+            </li>
+
+            <li>
+              O que você está procurando ?<br />
+              <label>
+                <button>Alugar</button>
+                <button>Comprar</button>
+              </label>
+            </li>
+          </ul>
+        </div>
       </nav>
     </div>
   );
