fb3b54fe41b27006ac28a14408b38e4a037cb257
Gabriel Rabelo <31012788+gabrielrab@users.noreply.github.com>
2019-08-18 11:56:05 -0300

    init "login" page

---
+++ src/pages/Login.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function() {
+  return (
+    <div>
+      <h1>Olá</h1>
+    </div>
+  );
+}
--- src/routes.js
+++ src/routes.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route } from "react-router-dom";
 
 import Main from "./pages/Main";
 import Busca from "./pages/Busca";
+import Login from "./pages/Login";
 
 export default function Routes() {
   return (
     <BrowserRouter>
       <Route path="/" exact component={Main} />
       <Route path="/busca" component={Busca} />
+      <Route path="/login" component={Login} />
     </BrowserRouter>
   );
 }
