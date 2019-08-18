aed5ce602cb651b559889fecc6c6ff95069de738
Gabriel Rabelo <31012788+gabrielrab@users.noreply.github.com>
2019-08-18 12:10:33 -0300

    finish login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,9 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "./Login.css";
+
+import logo from "../assets/logo.svg";
 
 export default function() {
   return (
-    <div>
-      <h1>Olá</h1>
+    <div className="login-container">
+      <form>
+        <Link to="/">
+          <img src={logo} alt="Nexo" />
+        </Link>
+        <input placeholder="Digite seu usuário" />
+        <input type="password" placeholder="Digite sua senha" />
+        <button type="submit">Enviar</button>
+      </form>
     </div>
   );
 }
