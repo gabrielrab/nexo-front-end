9c8af649e40aa6b1106c8d1ae03552317101b0d6
Gabriel Rabelo <31012788+gabrielrab@users.noreply.github.com>
2019-08-23 15:36:13 -0300

    Finish dashboard

--- src/pages/Dashborad.js
+++ src/pages/Dashborad.js
@@ -1,5 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import "./Dashboard.css";
+
+import logo from "../assets/logo.svg";
 
 export default function() {
-  return <h>Dashborad</h>;
+  return (
+    <div className="container">
+      <Link to="/">
+        <img src={logo} alt="Nexo" />
+      </Link>
+      <h1>Dashboard</h1>
+      <Link to="/create">
+        <button className="btn">Adicionar Anúncio</button>
+      </Link>
+      <div className="box-container">
+        <div className="box">
+          <span>#0001</span>
+          <h3>Casa Alto Padrão</h3>
+          <p>B. Vitória - Carmo do Cajuru</p>
+          <br />
+          <label>Adicionado em: 24/08/2019</label>
+          <div>
+            <Link to="/">
+              <button className="btn-das">Editar</button>
+            </Link>
+            <Link to="/">
+              <button className="btn-das">Remover</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
