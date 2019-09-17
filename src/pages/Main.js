ad1fa094ddbf18cc6eec90580a2a5ccd3f6aecf7
Gabriel Rabelo <contato.ideialisa@gmail.com>
2019-09-16 21:25:54 -0300

    Finish 16-07

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -21,27 +21,34 @@ export default function Main({ history }) {
     </label>
   );
   return (
-    <main>
-      <header>
-        <img src={logo} alt="Nexo" />
-        <Link to="/login">
-          <h3>Login</h3>
-        </Link>
-      </header>
-      <div className="container">
-        <h1>Busque pelos melhores imóveis da região.</h1>
-        <form className="box-search" onSubmit={handleSubmit}>
-          <h1>O que você está procurando?</h1>
-          <Label onClick={handleChange("text")} text="Comprar" />
-          <button className="btn-desabled">Alugar</button>
+    <>
+      <main>
+        <header>
+          <img src={logo} alt="Nexo" />
+          <Link to="/login">
+            <h3>Login</h3>
+          </Link>
+        </header>
+        <div className="container">
+          <h1>Busque pelos melhores imóveis da região.</h1>
+          <form className="box-search" onSubmit={handleSubmit}>
+            <h1>O que você está procurando?</h1>
+            <Label onClick={handleChange("text")} text="Comprar" />
+            <button className="btn-desabled">Alugar</button>
+            <br />
+            <button className="btn">Buscar</button>
+          </form>
+          <p>
+            Imóveis selecionados com alto padrão de qualidade e com a faixa de
+            preço que cabe no seu bolso.
+          </p>
+        </div>
+        <div className="foo">
+          <img src={logo} alt="Nexo" />
           <br />
-          <button className="btn">Buscar</button>
-        </form>
-        <p>
-          Imóveis selecionados com alto padrão de qualidade e com a faixa de
-          preço que cabe no seu bolso.
-        </p>
-      </div>
-    </main>
+          Desenvoldo por Gabriel Rabelo
+        </div>
+      </main>
+    </>
   );
 }
